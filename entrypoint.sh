20fcc29d7b3207dc056e47b869b68f0e98a12819
Amit021 <d.arghya93@gmail.com>
2024-06-11 23:13:33 +0200

    Updated sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # entrypoint.sh
 
 # Run migrations
-python manage.py makemigrations
+python manage.py makemigrations myapp
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
 # Start the server
